0e5c3a979ca780eec17ad8f8fb1861cddfad2362
Rogério Fontes <fontestz@gmail.com>
2018-03-11 20:22:11 -0300

    Projeto aula

---
+++ src/br/com/exemplo/infra/CarregaDriver.java
@@ -0,0 +1,24 @@
+package br.com.exemplo.infra;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class CarregaDriver {
+	public static void main(String args[]) {
+
+		try {
+			Class.forName("org.h2.Driver");
+			System.out.println("O driver foi carregado!");
+
+			Connection conn = DriverManager.getConnection("jdbc:h2:mem:dbaluno", "", "");
+			System.out.println("\nConexao Efeturada: " + conn);
+			
+		} catch (ClassNotFoundException e) {
+			System.out.println("Não consegui carregar o driver!" + e.getMessage());
+		} catch (SQLException ex) {
+			System.out.println("Erro na conexao: " + ex.getMessage());
+		}
+
+	}
+}
